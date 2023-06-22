9b4acc9fe9fe824c6e0a4be7c3cececb4e7aa2af
Hanif adedotun <hanifadedotun2k19@gmail.com>
2023-06-22 17:31:06 +0100

    landing page started

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import styles from './page.module.css';
 import Header from '@/components/header';
 import FooterSection from '@/components/footer';
+import HomeSection from '@/components/LandingPage/homeSection';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header/>
 
-      {/* <LandingPage/> */}
+      <HomeSection/>
 
       <FooterSection/>
     </main>
--- components/LandingPage/homeSection.tsx
+++ components/LandingPage/homeSection.tsx
@@ -1,8 +1,28 @@
+'use client';
+
+import Image from 'next/image';
+import { Button } from '../button';
+import styles from './landingPage.module.scss';
 
 export default function HomeSection() {
      return(
-          <div>
-               
-          </div>
+          <section className={styles.homeContainer}>
+               <div className={styles.textSection}>
+                    <h1>Fast and <span className="secondary">Unlimited</span> Internet</h1>
+
+                    <h3>Unlimited speed and freedom - no throttling, no limits.</h3>
+
+                    <Button describe='secondary'>Explore Bundles</Button>
+               </div>
+
+               <div className={styles.imageContainer}>
+                    <Image 
+                    src='/assets/mast.svg'
+                    width={'745'}
+                    height={'615'}
+                    alt="Image of a mast"
+                    />
+               </div>
+          </section>
      )
 }
