10922206825ad5c192b930e1d97de65047b51f4e
Hanif adedotun <hanifadedotun2k19@gmail.com>
2023-07-10 17:33:40 +0100

    done

--- components/faq/faq.ts
+++ components/faq/faq.ts
@@ -10,11 +10,43 @@ export const faq = [
           ]
      },
      {
-          question: "What do I need for the installation?",
+          question: "What is the cost of installation?",
           ans: [
-               "Radio Cost: The cost of the radio varies based on the distance between your house and our base station. Prices start from 31,000 and can go up to 55,000, subject to potential changes.",
-               "Access Point: The cost of the access point is fixed at 27,000.",
-               "Subscription Fee: The subscription fee differs depending on the package you choose. Click here to see more"
+               `Radio Cost: The cost of the radio varies based on the distance between your house and our base station. Prices start from 31,000 and can go up to 55,000, subject to potential changes.
+               
+               Access Point: The cost of the access point is fixed at 27,000.
+               
+               Subscription Fee: The subscription fee differs depending on the package you choose. Click here to see more`
           ]
-     }
+     },
+     {
+          question: "Can I pay in installments?",
+          ans: [
+               `Radio Cost: The cost of the radio varies based on the distance between your house and our base station. Prices start from 31,000 and can go up to 55,000, subject to potential changes.
+               
+               Access Point: The cost of the access point is fixed at 27,000.
+               
+               Subscription Fee: The subscription fee differs depending on the package you choose. Click here to see more`
+          ]
+     },
+     {
+          question: "What do I do when there is no internet? ",
+          ans: [
+               `Radio Cost: The cost of the radio varies based on the distance between your house and our base station. Prices start from 31,000 and can go up to 55,000, subject to potential changes.
+               
+               Access Point: The cost of the access point is fixed at 27,000.
+               
+               Subscription Fee: The subscription fee differs depending on the package you choose. Click here to see more`
+          ]
+     },
+     {
+          question: "How long does it take to install?",
+          ans: [
+               `Radio Cost: The cost of the radio varies based on the distance between your house and our base station. Prices start from 31,000 and can go up to 55,000, subject to potential changes.
+               
+               Access Point: The cost of the access point is fixed at 27,000.
+               
+               Subscription Fee: The subscription fee differs depending on the package you choose. Click here to see more`
+          ]
+     },
 ]
